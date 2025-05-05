2becb05112aca70429aa18d7f4c28a011915d2cd
gauravupadhyay <upadhyaygaurav572@gmail.com>
2025-05-06 01:05:12 +0530

    initial commit

---
+++ backand/routes/job.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+
+import isAuthenticated from "../middlewares/isAuthenticated.js"
+import { getAlljobs, getJobById, postjob } from "../controllers/job.controller.js";
+const router = express.Router();
+router.route("/post").post(isAuthenticated,postjob);
+router.route("/get").get(isAuthenticated,getAlljobs);
+router.route("/getadminjobs").get(isAuthenticated,getAlljobs);
+router.route("/get/:id").get(isAuthenticated,getJobById);
+export default router;
