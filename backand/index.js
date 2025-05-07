3c8633a5f3b50e69fcea8000d084d7a6eb207f4e
gauravupadhyaybiet <162415962+gauravupadhyaybiet@users.noreply.github.com>
2025-05-07 18:16:33 +0530

    Update index.js

--- backand/index.js
+++ backand/index.js
@@ -14,7 +14,7 @@ app.use(express());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const allowedOrigin = 'http://localhost:5173';
+const allowedOrigin = 'job-hunt-frontend-47jk.vercel.app';
 
 // Use the cors middleware with specific origin
 app.use(cors({
